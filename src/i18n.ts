a21bcd9b64190c638d0daf2c747c05009a6be925
doheon <heizence6626@gmail.com>
2025-08-28 21:50:15 +0900

    fix : solve ii18n library issue when build

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,23 +1,22 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
+import Backend from "i18next-http-backend";
 
 i18n
-  .use(LanguageDetector) // 사용자의 시스템 언어를 감지
-  .use(initReactI18next) // i18n 인스턴스를 react-i18next에 전달
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
   .init({
-    // 기본 언어 파일 경로
-    resources: {
-      en: {
-        translation: await fetch("../public/locales/en/texts.json").then((res) => res.json()),
-      },
-      ko: {
-        translation: await fetch("../public/locales/ko/texts.json").then((res) => res.json()),
-      },
+    backend: {
+      loadPath: "/locales/{{lng}}/texts.json",
     },
-    fallbackLng: "en", // 언어 감지 실패 시 기본 언어
+    fallbackLng: "en",
     interpolation: {
-      escapeValue: false, // React는 이미 XSS 방어 기능이 있으므로 false로 설정
+      escapeValue: false,
+    },
+    react: {
+      useSuspense: true,
     },
   });
 
